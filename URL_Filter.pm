15ccfacbb09c460168e57a770d9262c19ec2e270
KuroiZell <lp008962@hotmail.com>
2014-09-03 13:50:11 +0900

    Update URL_Filter.pm
    りふぁくたりんぐ

--- URL_Filter.pm
+++ URL_Filter.pm
@@ -1,110 +1,147 @@
-package YAMAHA::URL_Filter;
-
+package My::YAMAHA::URL_Filter;
 use strict;
 use warnings;
 use Class::Accessor 'antlers';
 use List::MoreUtils 'apply';
+### use My::DNS;
+
+my %engines = (
+    "Google"    => { 
+        pattern => 'google.co.jp/search\?',
+        regexp  => '^.+search.+?\&q\=(.+?)(&.+|)$',
+    },
+    "Yahoo"     => { 
+        pattern => 'http://search.yahoo.co.jp/',
+        regexp  => '^.+?search(\?p\=|.+?\&p\=|.+?\?p=)([^&].+?)(&search.+|&.+|)$',
+    },
+    "Cybonet"   => { 
+        pattern => '^http://search.cybozu.net/\?keywords',
+        regexp  => '^.+?\?keywords\=(.+?)(&.+|)$',
+    },
+    "Bing"      => { 
+        pattern => 'bing.com/search\?',
+        regexp  => '^.+search\?q\=(.+?)(&.+|)$',
+    },
+    "Wikipedia" => { 
+        pattern => 'wikipedia.org/wiki',
+        regexp  => '.+?wikipedia.org\/wiki\/(.+?)',
+    },
+);
 
 # Accessor
-has 'file' => (isa => 'Str', is => 'rw');
-#has 'model' => (isa => 'Str', is => 'rw'); # 他モデルのログ形式しらない
-#has 'ipaddr' => (isa => 'Str', is => 'rw'); # サーチ対象を絞るなら。
+has 'file'   => (isa => 'Str', is => 'rw');
+has 'result' => (isa => 'Str', is => 'rw');
+has 'ip'     => (isa => 'Str', is => 'rw');
 
+### my $dns = My::DNS->new;
 
 # Constractor
 sub new {
     my $proto = shift;
     my $class = ref $proto || $proto;
     my %args = @_;
-    my $self = {
-        file => $args{file} || '/var/log/yamaha/rtlog',
-# ipaddr => $args{ipaddr} || '192.168.1.',
-# model => $args{model} || 'rtx1100',
+    my $self  = { 
+        file   => $args{file} || '/var/log/yamaha/rtlog',
         result => '',
+        ip     => {},
     };
     return bless($self, $class);
 
 }
 
 
-# Method you use
-sub searched {
+sub get_searched_queries {
     my $self = shift;
-    my $prev = "";
+    my $url  = shift;
 
-    open my $fh, '<', $self->{file};
+    # Parse  a single-URL-line. Not by file
+    if ($url) {
+        return $self->parse_url($url);
+    } else {
+        $self->analyze($self->file);
+    }
+
+    return $self->result;
+}
+
+sub analyze {
+    my ($self, $file) = @_;
+
+    my $prev = "";
+    open my $fh, '<', $file or die "File Not Found $file: $!";
+
+    # Make regexp  to match lines
+    my $str_matching = "";
+    foreach my $key ( keys %engines ) {
+        $str_matching = sprintf("%s|", $engines{$key}{pattern});
+    }
+    $str_matching = substr($str_matching, 0, -1); # Delete a last pipe.
 
     while (<$fh>) {
+        next unless $_ =~ /$str_matching/oi;
         next if $_ eq $prev;
         $prev = $_;
-        next unless $_ =~ /\[URL_FILTER\]/;
-# next unless $_ =~ /$self->{ipaddr}/;
 
-        $self->analyze($_);
+        $self->parse_log($_);
     }
 
-    return $self->{result};
 }
 
-
-# Internal method
-sub analyze {
+sub parse_log {
     my ($self, $line) = @_;
 
-    # 完全にログ形式に依存
-    my @cols = split(' ', $line);
-    my $m = $cols[0];
-    my $d = $cols[1];
-    my $time = $cols[2];
-    my $ip = $cols[10];
-    my $url = $cols[12]; # is query_strings.
-    my $query;
+    my @columns = split(' ', $line);
+    my $month   = $columns[0];
+    my $day     = $columns[1];
+    my $time    = $columns[2];
+    my $ip      = $columns[10];
+    my $url     = $columns[12];
+    my $result  = "";
+    my $query   = "";
 
-    # Let's REGEXP!!!!!!!1111
-    # もっとマシな書き方ェ・・・・
-    if ( $url =~ q|google.co.jp/search\?|) {
-        $query = apply { s/^.+search.+?q\=(.+?)(&.+|)$/$1/g } $url;
-        $self->decode($m, $d, $time, $ip, $query, "Google");
+    $query = $self->parse_url( $url );
+    return if !$query;
+    
+    # 同じIPから二度DNSを引かないように
+    ### $self->{ip}{$ip} = $dns->ip2user($ip) if ( !$self->{ip}{$ip} );
 
-    } elsif ( $url =~ q|http://search.yahoo.co.jp/|) {
-        $query = apply { s/^.+?search(\?p\=|.+?\&p\=)([^&]*?)(&.+|)$/$2/g } $url;
-        $self->decode($m, $d, $time, $ip, $query, "Yahoo");
-
-    } elsif ( $url =~ q|^http://search.cybozu.net/\?keywords|) {
-        $query = apply { s/^.+?\?keywords\=(.+?)(&.+|)$/$1/g } $url;
-        $self->decode($m, $d, $time, $ip, $query, "Cybonet");
- 
-    } elsif ( $url =~ q|bing.com/search\?|) {
-        $query = apply{ s/^.+search\?q\=(.+?)(&.+|)$/$1/g } $url;
-        $self->decode($m, $d, $time, $ip, $query, "Bing");
- 
-    } elsif ( $url =~ q|wikipedia.org/wiki|) {
-        $query = apply { s/.+?wikipedia.org\/wiki\/(.+?)/$1/g } $url;
-        $self->decode($m, $d, $time, $ip, $query, "Wikipedia");
-    }
+   ###  $self->{result} .= sprintf("%2s %2s %5s : %-15s -> %s\n", 
+   ###                            $month, $day, $time, $self->{ip}{$ip}, $query );
+    $self->{result} .= sprintf("%2s %2s %5s : %-15s -> %s\n", 
+                                                    $month, $day, $time, $ip, $query );
 
 }
 
-sub decode {
-    my ($self, $m, $d, $time, $ip, $query, $engine) = @_;
-    return if $query =~ /^http/;
-    return if $query eq "";
+sub parse_url {
+    my ($self, $url) = @_;
+    my $query;
 
-    my $msg = "";
+    foreach my $key ( keys %engines ) {
+            if ($key eq 'Yahoo') {
+                $query = apply { s/$engines{$key}{regexp}/$2/g } $url;
+            } else {
+                $query = apply { s/$engines{$key}{regexp}/$1/g } $url;
+            }
 
-    if ($query =~ /%[0-9A-Fa-f]{2}/) {
-        $query =~ tr/+/ /;
-        $query =~ s/%([0-9A-Fa-f][0-9A-Fa-f])/pack('H2', $1)/eg;
-
-        $msg = sprintf("%s %s %s : %s \t -> %s (%s)\n", $m, $d, $time, $user, $query, $engine);
-
-    } else {
-
-        $msg = sprintf("%s %s %s : %s \t -> %s (%s)\n", $m, $d, $time, $user, $query, $engine);
+            $query = $self->decode($query);
+            $query = sprintf("%s(%s)", $query, $key);
 
     }
 
-    $self->{result} .= $msg;
+    return $query;
+}
+
+sub decode { 
+    my ($self, $query) = @_;
+
+    return if $query eq "";
+
+    if ($query =~ /%[0-9A-Fa-f]{2}/) {
+        use URI::Escape;
+        $query = uri_unescape( $query );
+    }
+
+    return $query;
 }
 
 1;
